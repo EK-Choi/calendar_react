941332a01a0fbf928763730f402a45b5c272d045
EK-Choi <77577929+EK-Choi@users.noreply.github.com>
2021-03-23 02:17:24 +0900

    layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>월</h1>
+      <div>MON</div>
+      <div>TUE</div>
+      <div>WED</div>
+      <div>THU</div>
+      <div>FRI</div>
+      <div>SAT</div>
+      <div>SUN</div>
 
     </div>
   );
