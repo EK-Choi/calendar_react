01b77d8bbf4b843e8f618ec1b06a0a8cd96cc574
EK-Choi <77577929+EK-Choi@users.noreply.github.com>
2021-03-23 18:18:57 +0900

    adding calendar with funtion

--- src/Calendar.js
+++ src/Calendar.js
@@ -1,31 +1,72 @@
 import React from "react";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import styled from "styled-components";
+import {useState} from 'react';
+import moment from 'moment';
 
 const Calendar = (props) => {
+  const [getMoment, setMoment]=useState(moment());     
+  const today = getMoment;
+
+  const firstWeek = today.clone().startOf('month').week();
+  const lastWeek = today.clone().endOf('month').week() === 1 ? 53 : today.clone().endOf('month').week();
+
+  const calendarArr=()=>{
+
+    let result = [];
+    let week = firstWeek;
+    for ( week; week <= lastWeek; week++) {
+      result = result.concat(
+        <tr key={week}>
+          {
+            Array(7).fill(0).map((data, index) => {
+              let days = today.clone().startOf('year').week(week).startOf('week').add(index, 'day');
+
+              return(
+                  <td key={index}>
+                    <span>{days.format('D')}</span>
+                  </td>
+              );
+            })
+          }
+        </tr>);
+    }
+    return result;
+  }
+
   return (
     <AppStyle>
       <Head>
-        <button><FiChevronLeft/></button>
-        <h2>March 2021</h2>
-        <button><FiChevronRight/></button>
+        <button onClick={()=>{ setMoment(getMoment.clone().subtract(1, 'month')) }}><FiChevronLeft/></button>
+        <h2>{today.format('MMM YYYY')}</h2>
+        <button onClick={()=>{ setMoment(getMoment.clone().add(1, 'month')) }}><FiChevronRight/></button>
       </Head>
-      <div className="body">
-        <div>MON</div>
-        <div>TUE</div>
-        <div>WED</div>
-        <div>THU</div>
-        <div>FRI</div>
-        <div>SAT</div>
-        <div>SUN</div>
-      </div>
+      <Body>
+        <Weekdays>
+          <div>SUN</div>
+          <div>MON</div>
+          <div>TUE</div>
+          <div>WED</div>
+          <div>THU</div>
+          <div>FRI</div>
+          <div>SAT</div>
+        </Weekdays>
+        
+        <table>
+          <tbody>
+            {calendarArr()}
+          </tbody>
+        </table>
+      </Body>
 
-      </AppStyle>
+    </AppStyle>
   )
 };
 
 const AppStyle = styled.div`
   background-color: rgb(234, 244, 250);
+  padding-bottom: 1px;
+  height: 100vh;
 `;
 
 const Head = styled.div`
@@ -49,6 +90,7 @@ const Head = styled.div`
     border-radius: 30px;
     padding-top: 4px;
     color: gray;
+    cursor: pointer;
     :hover{
       background-color: rgb(200, 200, 200);
       color: white;
@@ -56,4 +98,38 @@ const Head = styled.div`
   }
 `;
 
+const Body = styled.div`
+  margin: 4%;
+  background-color: rgb(255, 255, 253);
+
+  & table{
+    width: 100%;
+    height: 75vh;
+  }
+  
+`;
+
+const Weekdays = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  & div {
+    width: 20%;
+    text-align: center;
+    font-weight: bold;
+    color: gray;
+
+    :first-child, :last-child{
+      color: rgb(238, 123, 123);
+    }
+  }
+
+`;
+
+const Dates = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 export default Calendar;
